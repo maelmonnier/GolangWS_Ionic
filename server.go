f49fc30cb51599ee9b1eb17704bbe17029137d93
Mael Monnier <mael.monnier@laposte.net>
2017-03-24 10:59:03 +0100

    Add access control

--- server.go
+++ server.go
@@ -7,14 +7,33 @@ import (
 )
 
 type User struct {
-	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Name  string `json:"username" xml:"username" form:"username" query:"username"`
 	Email string `json:"email" xml:"email" form:"email" query:"name"`
+}
+
+// ServerHeader middleware adds a `Server` header to the response.
+func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderServer, "Echo/3.0")
+		return next(c)
+	}
 }
 
 func main() {
 	e := echo.New()
+
+	// Server header
+	e.Use(ServerHeader)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.OPTIONS("/users", func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderAllow, "POST")
+		c.Response().Header().Set(echo.HeaderAccessControlAllowOrigin, "http://localhost:8000")
+		c.Response().Header().Set(echo.HeaderAccessControlAllowHeaders, "Content-Type")
+		return c.String(http.StatusOK, "Just post of a new user for the moment")
 	})
 
 	e.POST("/users", func(c echo.Context) error {
